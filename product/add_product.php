b9aa11a4a22367f6516f13d0a80a4c12508cb021
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-28 16:06:55 +0600

    add product page added

---
+++ product/add_product.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Product</title>
+</head>
+<body>
+    <form action="<?php $_SERVER['PHP_SELF'];?>" method="post">
+
+    </form>
+</body>
+</html>
