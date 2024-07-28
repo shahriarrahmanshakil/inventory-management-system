27f263665f9544d8dc91933e349de97755d3a52f
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-28 16:50:17 +0600

    sql queries in add product file

--- product/add_product.php
+++ product/add_product.php
@@ -1,3 +1,23 @@
+<?php
+    require("../connect_db.php");
+
+    if(isset($_POST['btnsubmit'])){
+        $product_name       = $_POST['product_name'];
+        $product_category   = $_POST['product_category'];
+        $product_code       = $_POST['product_code'];
+        $product_entry_date = $_POST['product_entry_date'];
+        
+        $insert_product = "INSERT INTO product(product_name,product_category,product_code,product_entry_date)
+                            VALUES('$product_name',' $product_category','$product_code','$product_entry_date')";
+        $insert_product_query = $conn->query($insert_product);
+    }
+?>
+
+<?php
+
+     
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +27,31 @@
 </head>
 <body>
     <form action="<?php $_SERVER['PHP_SELF'];?>" method="post">
+        <label for="product_name">Product Name</label><br>
+        <input type="text" id="product_name" name="product_name"><br><br>
 
+        <label for="product_category">Product Category</label><br>
+        <select name="product_category" id="product_category">
+            <?php 
+                $select_category = "SELECT * FROM category";
+                $select_category_query = $conn->query($select_category);
+
+                while($category_array = mysqli_fetch_assoc($select_category_query)){
+                    $category_id = $category_array['category_id'];
+                    $category_name = $category_array['category_name'];
+
+                    echo "<option value=' $category_id'>$category_name</option>";
+                }      
+            ?>
+        </select> <br><br>
+
+        <label for="product_code">Product Code</label><br>
+        <input type="text" id="product_code" name="product_code"><br><br>
+
+        <label for="product_entry_date">Product Entry Date</label><br>
+        <input type="date" id="product_entry_date" name="product_entry_date"><br><br>
+
+        <input type="submit" name="btnsubmit" value="Submit">
     </form>
 </body>
 </html>
