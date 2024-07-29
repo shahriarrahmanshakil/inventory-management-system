a9c9c4f843b0459a77b03a99fcd63ae15562d2b4
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-29 11:06:57 +0600

    add store product added

---
+++ store_product/add_store_product.php
@@ -0,0 +1,38 @@
+<?php
+    require("../connect_db.php");
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Store Product</title>
+</head>
+<body>
+    <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
+        <label for="store_product">Store Product</label><br>
+        <select name="store_product" id="store_product">
+            <?php
+                $select_product = "SELECT * FROM product"; 
+                $select_product_query = $conn->query($select_product);
+                while($store_product_array = mysqli_fetch_assoc($select_product_query)){
+                    $product_id = $store_product_array['product_id'];
+                    $product_name = $store_product_array['product_name'];
+    
+                    echo "<option value='$product_id'> $product_name</option>";
+                }
+
+            ?>
+        </select><br><br>
+        <label for="s_p_q">Store Product Quantity</label><br>
+        <input type="text" id="s_p_q" name="s_p_q"><br><br>
+        <label for="s_p_entry_date">Store Product Entry Date</label><br>
+        <input type="date" id="s_p_entry_date" name="s_p_entry_date"><br><br>
+        <input type="submit" name="btnsubmit" value="Submit">
+    </form>
+</body>
+</html>
