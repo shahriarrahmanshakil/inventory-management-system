4742e8ac8dd2c2aa5d92c52c9bee80afa1af5319
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-31 16:20:45 +0600

    add spend product init

--- spend_product/add_spend_product.php
+++ spend_product/add_spend_product.php
@@ -1,4 +1,19 @@
 <?php
+    require("../connect_db.php");
+
+    if(isset($_POST['btnsubmit'])){
+        $spend_product_name = $_POST['spend_product'];
+        $spend_product_quantity = $_POST['spend_product_quantity'];
+        $spend_product_entry_date = $_POST['spend_product_entry_date'];
+
+        $insert_spend_product = "INSERT INTO spend_product(spend_product_name, spend_product_quantity, spend_product_entry_date)
+                                VALUES('$spend_product_name', ' $spend_product_quantity', '$spend_product_entry_date')";
+        $spend_product_query = $conn->query($insert_spend_product);
+
+        if(  $spend_product_query == TRUE){
+            echo "Spend Data Inserted";
+        }
+    }
 
 ?>
 
@@ -12,7 +27,19 @@
 <body>
     <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
         <label for="spend_product">Spend Product Name</label><br>
-        <input type="text" id="spend_product" name="spend_product"><br><br>
+        <select name="spend_product" id="spend_product">
+            <?php
+                $view_product = "SELECT * FROM product";
+                $view_product_query = $conn->query($view_product);
+
+                while($view_product_array = mysqli_fetch_assoc($view_product_query)){
+                    $product_id = $view_product_array['product_id'];
+                    $product_name = $view_product_array['product_name'];
+
+                    echo "<option value='$product_id'>$product_name</option>" ;
+                }
+            ?>
+        </select><br><br>
 
         <label for="spend_product_quantity">Spend Product Quantity</label><br>
         <input type="text" id="spend_product_quantity" name="spend_product_quantity"><br><br>
