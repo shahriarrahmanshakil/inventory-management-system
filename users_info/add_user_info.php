d3ca72783b8c9b8b579a779c9421b2ac85605201
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-08-02 21:05:14 +0600

    user form added

--- users_info/add_user_info.php
+++ users_info/add_user_info.php
@@ -6,8 +6,14 @@
     <title>Add User Info</title>
 </head>
 <body>
-    <form action="<?php $_SERVER['SERVER_SELF']?>" method="post">
-        
+    <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+        <label for="user_name">User Name</label><br>
+        <input type="text" id="user_name" name="user_name"><br><br>
+        <label for="user_email">User Email</label><br>
+        <input type="email" id="user_email" name="user_email"><br><br>
+        <label for="user_password">User Password</label><br>
+        <input type="password" id="user_password" name="user_password"><br><br>
+        <input type="submit" name="btnsubmit" value="Login">
     </form>
 </body>
 </html>
