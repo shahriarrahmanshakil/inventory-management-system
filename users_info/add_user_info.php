c633560b12fd44ca5b463160efdfc4d7c8ac5b92
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-08-02 20:56:24 +0600

    user_info added

---
+++ users_info/add_user_info.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add User Info</title>
+</head>
+<body>
+    <form action="<?php $_SERVER['SERVER_SELF']?>" method="post">
+        
+    </form>
+</body>
+</html>
