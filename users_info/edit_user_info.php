fa6fc8a55bfbedf7deeb5498121ed5423e82e17e
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-08-02 23:42:40 +0600

    update query added in edit user info

--- users_info/edit_user_info.php
+++ users_info/edit_user_info.php
@@ -8,10 +8,27 @@
         $query_edit_user = $conn->query($edit_user_info_sql);
         $array_edit_user = mysqli_fetch_assoc($query_edit_user);
 
-            $user_id = $array_edit_user['user_id'];
-            $user_name = $array_edit_user['user_name'];
-            $user_email = $array_edit_user['user_email'];
-            $user_password = $array_edit_user['user_password'];
+            $user_id        = $array_edit_user['user_id'];
+            $user_name      = $array_edit_user['user_name'];
+            $user_email     = $array_edit_user['user_email'];
+            $user_password  = $array_edit_user['user_password'];
+    }
+?>
+
+<?php
+    if(isset($_POST['btnsubmit'])){
+        $new_user_id       = $_POST['user_id'];   
+        $new_user_name     = $_POST['user_name']; 
+        $new_user_email    = $_POST['user_email']; 
+        $new_user_password = $_POST['user_password']; 
+
+        $update_user_info = "UPDATE users_info
+                            SET user_name='$new_user_name',
+                                user_email='$new_user_email',
+                                user_password='$new_user_password'
+                            WHERE user_id=$new_user_id";
+
+        $query_update_user_info = $conn->query($update_user_info);
     }
 ?>
 
@@ -30,6 +47,7 @@
         <input type="email" id="user_email" name="user_email" value="<?php echo $user_email?>"><br><br>
         <label for="user_password">User Password</label><br>
         <input type="password" id="user_password" name="user_password" value="<?php echo $user_password?>"><br><br>
+        <input type="text" id="user_id" name="user_id" value="<?php echo $user_id ?>" hidden><br><br>
         <input type="submit" name="btnsubmit" value="Update">
     </form>
 </body>
