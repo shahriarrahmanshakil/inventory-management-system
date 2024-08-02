daa5ac36222b64056b8312f4d93fbca727893f6d
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-08-02 23:16:50 +0600

    update edit_user_info page

--- users_info/edit_user_info.php
+++ users_info/edit_user_info.php
@@ -15,3 +15,24 @@
 
     }
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add User Info</title>
+</head>
+<body>
+    <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+        <label for="user_name">User Name</label><br>
+        <input type="text" id="user_name" name="user_name"><br><br>
+        <label for="user_email">User Email</label><br>
+        <input type="email" id="user_email" name="user_email"><br><br>
+        <label for="user_password">User Password</label><br>
+        <input type="password" id="user_password" name="user_password"><br><br>
+        <input type="submit" name="btnsubmit" value="Login">
+    </form>
+</body>
+</html>
